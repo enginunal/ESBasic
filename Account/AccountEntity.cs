5c9b085ec6fcb9bfd37f96162865bd16be33b995
Engin ÜNAL <enginunal@gmail.com>
2022-02-28 14:28:11 +0300

    Add files via upload

---
+++ Account/AccountEntity.cs
@@ -0,0 +1,62 @@
+﻿using AccountEvents;
+using System;
+using System.Collections.Generic;
+
+namespace AccountDomain
+{
+    public class AccountEntity
+    {        
+        private Guid _id;
+        public decimal Balance { get; private set; }
+
+        public AccountEntity(Guid id)
+        {
+            Raise(new AccountEvents.Created(id));
+        }
+
+        public void Deposit(int amount)
+        {
+            Raise(new AccountEvents.Deposited(_id, amount));
+        }
+        public void Withdraw(int amount)
+        {
+            Raise(new AccountEvents.Withdrawn(_id, amount));
+        }
+
+        private void Apply(AccountEvents.Created @event)
+        {
+            _id = @event.AccountId;
+        }
+        private void Apply(AccountEvents.Deposited @event)
+        {
+            Balance += @event.Amount;
+        }
+        private void Apply(AccountEvents.Withdrawn @event)
+        {
+            Balance -= @event.Amount;
+        }
+                       
+        public AccountEntity(IEnumerable<object> history){
+            foreach (var @event in history)
+            {
+                Apply(@event as dynamic); 
+            }
+        }
+
+        private List<object> _eventStore = new List<object>(); 
+        
+        private void Raise(object @event)
+        {
+            _eventStore.Add(@event);
+            Apply(@event as dynamic); 
+        }
+
+        public List<object> TakeEvents()
+        {
+            var @events = new List<object>(_eventStore);
+            return @events;
+        }
+
+
+    }
+}
---
+++ Account/AccountEvents.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountEvents
+{
+    public record Created
+    {
+        public readonly Guid AccountId;
+        public Created(Guid accountId)
+        {
+            AccountId = accountId;
+        }
+    }
+    public record Deposited
+    {
+        public readonly Guid AccountId;
+        public readonly int Amount;
+        public Deposited(Guid accountId, int amount)
+        {
+            AccountId = accountId;
+            Amount = amount;
+        }
+    }
+    public record Withdrawn
+    {
+        public readonly Guid AccountId;
+        public readonly int Amount;
+        public Withdrawn(Guid accountId, int amount)
+        {
+            AccountId = accountId;
+            Amount = amount;
+        }
+    }
+
+}
