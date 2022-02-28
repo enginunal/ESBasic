02b8336fc8e6cbaa0aeef4f4c39b124f8ed888fd
Engin ÜNAL <enginunal@gmail.com>
2022-02-28 14:28:59 +0300

    Add files via upload

---
+++ Account.App/Program.cs
@@ -0,0 +1,19 @@
+﻿using AccountDomain;
+
+
+//Create an account 
+var accountId = Guid.NewGuid();
+var account = new AccountEntity(accountId);
+//Some transactions
+account.Deposit(15);
+account.Deposit(5);
+account.Withdraw(10);
+//Account balance :
+Console.WriteLine($"Balance:{account.Balance}");
+
+//Now we have all necessary data to generate account from the ground up
+var @events = account.TakeEvents();
+var accountProjected = new AccountEntity(@events);
+Console.WriteLine($"Balance:{accountProjected.Balance}");
+
+
